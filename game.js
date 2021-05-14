87e0054d91f10e6882589c32536106a756aa50d8
Camron Braden <CamronRBraden@gmail.com>
2021-05-14 12:37:05 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -61,28 +61,28 @@ const loop = function () {
         player.yVelocity -= 0.75;
         player.xVelocity += 0.75;
         direction = 9;
-    } else if (controller.up && controller.left) {
+    } if (controller.up && controller.left) {
         player.yVelocity -= 0.75;
         player.xVelocity -= 0.75;
         direction = 7;
-    } else if (controller.up) {
+    } if (controller.up) {
         player.yVelocity -= 1.1;
         direction = 8;
-    }  else if (controller.down && controller.right) {
+    }  if (controller.down && controller.right) {
         player.yVelocity += 0.75;
         player.xVelocity += 0.75;
         direction = 3;
-    } else if (controller.down && controller.left) {
+    } if (controller.down && controller.left) {
         player.yVelocity += 0.75;
         player.xVelocity -= 0.75;
         direction = 1;
-    } else if (controller.down) {
+    } if (controller.down) {
         player.yVelocity += 1.1;
         direction = 2;
-    }  else if (controller.left) {
+    }  if (controller.left && !controller.up && !controller.down) {
         player.xVelocity -= 1.1;
         direction = 4;
-    } else if (controller.right) {
+    } if (controller.right && !controller.up && !controller.down) {
         player.xVelocity += 1.1;
         direction = 6;
     } 
